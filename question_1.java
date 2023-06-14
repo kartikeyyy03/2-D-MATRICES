fa02e9533ed30f9f18a285bef129685a19773935
Kartikeya Kandpal <kandpalkartikeya93@gmail.com>
2023-06-14 23:36:09 +0530

    sum of square matrix

---
+++ question_1.java
@@ -0,0 +1,34 @@
+class question_1{
+    public static void main(String[] args) {
+
+
+        int[][] arr1={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+                       };
+
+
+        int[][] arr2={
+            {9,8,7},
+            {6,5,4},
+            {3,2,1},
+        };
+
+
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+
+        System.out.print(arr1[i][j]+arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+        
+        
+        
+        
+        
+}
