dfde63872795a3893f012d614f7e6fbd64ad4bfd
Kartikeya Kandpal <kandpalkartikeya93@gmail.com>
2023-06-15 14:23:59 +0530

    Sum of column elements

---
+++ question_3.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class question_3 {
+
+    public static void main(String[] args) {
+        
+        int[][] array={
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,2,3,4}
+        };
+
+
+        int[] sumarray = new int[array[0].length];
+
+        for(int i=0;i<array[0].length;i++){
+            int sum=0;
+            for(int j=0;j<array.length;j++){
+
+                sum=sum+array[j][i];
+                sumarray[i]=sum;
+            }
+
+            System.out.print(sumarray[i]+ " ");
+
+
+
+
+        }
+
+        
+
+
+
+
+        
+    }
+    
+}
